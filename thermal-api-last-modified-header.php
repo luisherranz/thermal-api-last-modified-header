0cc814bfb9d7cc79ce4c54ddfcd28f7446516032
LuisHerranz <luisherranz@gmail.com>
2013-11-07 17:58:03 +0100

    Last-Modified header using get_lastpostmodified WP function

---
+++ thermal-api-last-modified-header.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Thermal API Last-Modified Header
+Plugin URI: -
+Description: Add all the Last-Modified to the `meta` output of the Thermal API plugin
+Author: Luis Herranz
+Author URI: -
+Version: 1.0.0
+*/
+
+add_filter( 'thermal_response',  function(&$res, &$app, &$data ) {
+		
+        $wp_last_mod_time = strtotime(get_lastpostmodified('gmt'));
+        $wp_last_mod_time = strtotime(get_lastpostmodified('gmt'));
+        $wp_last_mod_date = date( "D, d M Y H:i:s \G\M\T", $wp_last_mod_time );
+
+        $res->header( 'Last-Modified', $wp_last_mod_date );
+
+    	return $res;
+
+        }, 10, 3);
+?>
